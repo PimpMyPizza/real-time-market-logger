131596187d8fac35195b620dde9b5c1e53e906f8
Christophe Stilmant <Christophe.Stilmant@gmx.de>
2021-01-29 11:59:49 +0100

    datetime in filename

--- start.py
+++ start.py
@@ -1,9 +1,7 @@
-import os 
-
-i = 0
+import os
+from datetime import datetime
 
 while True:
-    print(i)
+    print(datetime.now().strftime("%d/%m/%Y %H:%M"))
     os.system('dart run')
-    os.system('cp log.txt bybit_log_'+str(i)+'.txt')
-    i = i + 1
+    os.system('cp log.txt bybit_log__'+datetime.now().strftime("%d_%m_%Y__%H_%M_%S")+'.txt')
