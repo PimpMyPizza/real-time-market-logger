8686a0be1490dfe5ee1ae4333c8fd7f7330ac2d9
Christophe Stilmant <Christophe.Stilmant@gmx.de>
2021-01-30 12:04:05 +0100

    YMD instead of DMY in filename loul

--- start.py
+++ start.py
@@ -4,4 +4,4 @@ from datetime import datetime
 while True:
     print(datetime.now().strftime("%d/%m/%Y %H:%M"))
     os.system('dart run')
-    os.system('cp log.txt bybit_log__'+datetime.now().strftime("%d_%m_%Y__%H_%M_%S")+'.txt')
+    os.system('cp log.txt logs/bybit_log__'+datetime.now().strftime("%Y_%m_%d__%H_%M_%S")+'.txt')
